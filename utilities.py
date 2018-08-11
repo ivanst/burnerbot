e4413a5df615208097901d9cb22825b4b0dfc6d3
Ivan Stanojevic <ivan@lightspeedretail.com>
2018-08-10 19:13:59 -0700

    Get utilities more orderly and working

--- utilities.py
+++ utilities.py
@@ -4,10 +4,11 @@ import logging
 import random
 import pygame
 import time
-import audioop
 import math
 import struct
-import numpy
+import numpy as np
+import wave
+import pyaudio
 
 
 def select_random_file_from_directory(path, last_file=None):
@@ -54,29 +55,40 @@ def background_sound_player(sound_files_path=None, start=0, play_file=None,
     pygame.mixer.music.set_volume(settings.BG_VOLUME)
     pygame.mixer.music.load(sound_file)
     pygame.mixer.music.set_endevent(24)
-    pygame.mixer.music.play(loops=0, start=start/1000)
+    pygame.mixer.music.play(loops=0, start=start / 1000)
+    logging.info("Background sound player playing " + sound_file)
     return sound_file
 
 
-def interrupt_sound_player(background_sound_file, interrupt_sound="test",
-                           fade_time=settings.FADE_TIME):
+def interrupt_background_sound(background_sound_file,
+                               fade_time=settings.FADE_TIME):
     """
-    Stop the pygame.music. Play background_sound_file. Restart music where we
-    left off.
+    Stop the pygame.music.play background sound and return stopping position
+        and filename.
     Params:
         background_sound_file: The sound file that was being played
         interrupt_sound: The sound file to interrupt with
         fade_time: How long to fade
     """
-    position = pygame.mixer.music.get_pos() + 3000
+    print(background_sound_file)
+    position = pygame.mixer.music.get_pos() + fade_time
     logging.info("Stopping music position is " + str(position))
     pygame.mixer.music.set_endevent()
     pygame.mixer.music.fadeout(fade_time)
     logging.info("beginning " + str(fade_time) + " millisecond fade")
-    time.sleep(fade_time/1000)
-    pygame.mixer.music.load("interrupt_sound/phone.ogg")
+    time.sleep(fade_time / 1000)
+    return position, background_sound_file
+
+
+def play_foreground_sound(sound_file="interrupt_sound/phone.ogg"):
+    pygame.mixer.music.load(sound_file)
     pygame.mixer.music.play()
-    time.sleep(5)
+
+
+def resume_background_sound(position, background_sound_file):
+    # first make sure nothing is playing
+    while pygame.mixer.music.get_busy():
+        time.sleep(.25)
     background_sound_player(play_file=background_sound_file, start=position)
 
 
@@ -89,25 +101,66 @@ def get_rms(sound_block):
         sound_block: The sound block to sample from
     """
 
-    count = len(sound_block)/2
-    format = "%dh"%(count)
+    count = len(sound_block) / 2
+    format = "%dh" % (count)
     shorts = struct.unpack(format, sound_block)
 
     # iterate over the block.
     sum_squares = 0.0
     for sample in shorts:
-    # sample is a signed short in +/- 32768.
-    # normalize it to 1.0
-        n = sample * 1.0/32768.0
-        sum_squares += n*n
+        # sample is a signed short in +/- 32768.
+        # normalize it to 1.0
+        n = sample * 1.0 / 32768.0
+        sum_squares += n * n
 
-    return math.sqrt( sum_squares / count )
+    return math.sqrt(sum_squares / count)
 
 
-def volume_trigger(sound, length, max_volume):
+def frequency_volume_trigger(frequency_dictionary, frequency_ignore_dictionary,
+                             sample):
     """
-    If a volume is exceeded this function evaluates true and can activate other functions
+    If ALL frequencies in the dictionary exceed accompanying amplitude, returns
+    true
+
+    :param frequency_dictionary: A dictionary containing the frequency position
+        and trigger amplitude at which to trigger. All must be exceeded to
+        trigger.
+    :param frequency_ignore_dictionary: A dictionary containing the frequency
+        position and amplitudes at which we should ignore the sound. All must
+        be observed as over limit to ignore.
+    :param sample: Sound sample to evaluate
     """
-    volume = audioop.max(sound, length)
-    if volume > max_volume:
-        pygame.mixer.music.set_endevent(pygame.constants.USEREVENT + 1)
+    ignore_tests = 0
+    for position, amplitude in frequency_ignore_dictionary.items():
+        if sample[position] > amplitude:
+            ignore_tests = ignore_tests + 1
+    if ignore_tests == len(frequency_ignore_dictionary):
+        logging.info("We just heard a bunch of noise. Ignoring...")
+        return False
+
+    for position, amplitude in frequency_dictionary.items():
+        if sample[position] < amplitude:
+            return False
+
+    return True
+
+
+def averaged_spectrogram(np_data_array, divisions=60):
+    spectrogram_array = []
+    num_chunk_elements = np_data_array.size // divisions
+    i = 0
+    while i < divisions:
+        chunk_array = np_data_array[i*num_chunk_elements:(i+1)*num_chunk_elements]
+        spectrogram_array.append(int(np.mean(chunk_array) / 1000))
+        i = i+1
+    return spectrogram_array
+
+
+def save_audio_data(audio_data):
+    logging.info("Saving audio to file")
+    cursor = wave.open("sound.wav", "wb")
+    cursor.setnchannels(2)
+    cursor.setsampwidth(pyaudio.get_sample_size(pyaudio.paInt16))
+    cursor.setframerate(44100)
+    cursor.writeframes(b''.join(audio_data))
+    cursor.close()
