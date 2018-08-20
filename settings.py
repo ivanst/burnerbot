8adb7fcc805c8b43e72968c932e4a0e8c8cb826c
iRobot <ivanst@gmail.com>
2018-08-20 05:53:16 +0000

    Add SYNTH_VOLUME_MINIMUM

--- settings.py
+++ settings.py
@@ -1,29 +1,27 @@
 # Devices
 CLEAN_MIC = 7
 SYNTH_MIC = 6
-MIXED_OUT = 7
+SYNTH_OUT = 6
 
 # How long to fade music
 FADE_TIME = 1000
 # Background volume level
 BG_VOLUME = 1.0
 SYNTH_VOLUME = 100
-# Wait this long after levels drop below "speaking" levels
-END_SPEAKING_TIME = 1
+SYNTH_VOLUME_MINIMUM = 30
 
 RECORDING_BUFFER_SIZE = 30
 RECORD_MAX_VOLUME = 500
 RECORD_MAX_SAMPLE_LENGTH = 2
 
 # Speaker detection
-SPEAKER_BEGIN_CYCLES = 5
-SPEAKER_END_CYCLES = 5
+SPEAKER_BEGIN_CYCLES = 3
+SPEAKER_END_CYCLES = 9
 SPEAKER_MAX_CYCLES = 1000000
 
 # Spectrum position and level trigger key
 SPECTRUM_TRIGGER_LEVELS = {
-    2: 20,
-    3: 20,
+    1: 20,
 }
 
 # Spectrum that we want to ignore by. Anything detected in these levels will
