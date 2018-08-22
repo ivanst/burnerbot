da3de119d51898b6aa9ada4e63a0deff6ea45a39
iRobot <ivanst@gmail.com>
2018-08-22 19:01:45 +0000

    New settings config

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ SYNTH_OUT = 6
 FADE_TIME = 1000
 # Background volume level
 BG_VOLUME = 1.0
+BG_VOLUME_MINIMUM = .2
 SYNTH_VOLUME = 100
 SYNTH_VOLUME_MINIMUM = 30
 
@@ -15,19 +16,20 @@ RECORD_MAX_VOLUME = 500
 RECORD_MAX_SAMPLE_LENGTH = 2
 
 # Speaker detection
-SPEAKER_BEGIN_CYCLES = 3
-SPEAKER_END_CYCLES = 9
+SPEAKER_BEGIN_CYCLES = 2
+SPEAKER_END_CYCLES = 6
 SPEAKER_MAX_CYCLES = 1000000
 
 # Spectrum position and level trigger key
 SPECTRUM_TRIGGER_LEVELS = {
-    1: 20,
+    1: 1000,
+    2: 1000
 }
 
 # Spectrum that we want to ignore by. Anything detected in these levels will
 # cause us to act as if nobody is talking. It should just be bad noise caught
 # here
 SPECTRUM_IGNORE_LEVELS = {
-    15: 60,
-    16: 60,
+    15: 9000,
+    16: 9000,
 }
